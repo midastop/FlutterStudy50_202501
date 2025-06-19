ec445168bfe1b7de87294739855b6adc6009f6ad
midastop <midastop@naver.com>
2025-06-19 18:07:39 +0900

    dart ch09_new30_syntax 까지 완료

--- DartStudy/dart_study02/bin/ch08_asynchronous/ch0807_stream02.dart
+++ DartStudy/dart_study02/bin/ch08_asynchronous/ch0807_stream02.dart
@@ -2,16 +2,16 @@
 import 'dart:async';
 
 void main() {
-  /* StreamController를 사용해 스트림을 제어하는 클래스로
-   * Stream에 데이터를 추가하거나 Stream에서 데이터를 읽어올 수 있다.
-   **/
-  final controller =  StreamController();
+  /* StreamController는 Stream을 생성하고 관리하는 클래스로
+   * Stream에 데이터를 추가하거나 Stream에서 데이터를 읽어올((구독할) 수 있다.
+   **/
+  final controller = StreamController();
   final stream1 = controller.stream;
   final streamListener1 = stream1.listen((val) {
     print(val);
   });
 
-  /* StreamController를의 add() 함수와 sink 속성의 add() 함수를 사용해 Stream에
+  /* StreamController의 add() 함수와 sink 속성의 add() 함수를 사용해 Stream에
    * 데이터를 추가할 수 있다. 일반적으로 사용할 때는 간편한 add() 함수를 이용하고 고성
    * 처리, 실시간 처리나 네트워크 응답 등에는 sink 속성의 add()를 사용하는 것이 좋다.
    **/
--- DartStudy/dart_study02/bin/ch08_asynchronous/ch0809_stream04.dart
+++ DartStudy/dart_study02/bin/ch08_asynchronous/ch0809_stream04.dart
@@ -1,5 +1,4 @@
 // 스트림(Stream) - Stream을 반환하는 함수
-
 /* Stream을 반환하는 함수의 반환 값은 Stream<T>와 같이 제네릭을 사용한다. 아래 함수에서
  * Stream에 전달되는 연속적인 값을 정수로 반환하므로 제네릭은 <int>를 사용했다. 그리고
  * 앞에서 Future를 반환하는 함수를 정의할 때 async 키워드를 사용했지만 Stream을 반환하는
@@ -7,7 +6,7 @@
  * 키워드를 사용해 값을 반환하면 된다.
  **/
 Stream<int> numbers() async* {
-  for(int i = 0; i < 5; i++) {
+  for (int i = 0; i < 5; i++) {
     yield i + 1;
     await Future.delayed(Duration(seconds: 1));
   }
@@ -15,5 +14,7 @@ Stream<int> numbers() async* {
 
 void main() {
   // 앞에서와 마찬가지로 Stream의 데이터는 listen() 함수에서 받아 처리한다.
-  numbers().listen((val) { print(val); });
+  numbers().listen((val) {
+    print(val);
+  });
 }
