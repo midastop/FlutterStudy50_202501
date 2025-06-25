a38c11f2ccc69076d91c9ad8b4d5e2ad1e88d585
midastop <midastop@naver.com>
2025-06-25 12:19:47 +0900

    basic_widgets의 row&column 참고사이트 추가

--- FlutterStudy/ch05/basic_widgets/lib/widgets/w05_rowwidget.dart
+++ FlutterStudy/ch05/basic_widgets/lib/widgets/w05_rowwidget.dart
@@ -11,6 +11,7 @@ class RowWidgetExam extends StatelessWidget {
         // children 파라미터에 여러 개의 자식 위젯을 리스트에 담아서 지정하면 됨
         // Row와 Column은 주축(main axis)과 교차축(cross axis)의 개념이 있고
         // Row의 주축은 가로 방향이며 교차축은 세로 방향을 의미 함.
+        // https://docs.flutter.dev/ui/layout#lay-out-multiple-widgets-vertically-and-horizontally
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
